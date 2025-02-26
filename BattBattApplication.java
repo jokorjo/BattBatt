d66e5ccf8d9b29c2be1eb3a5daa48ebcce20b0e6
jokorjo <joni.korkola@gmail.com>
2025-02-26 18:35:51 +0200

    Create BattBattApplication.java

---
+++ BattBattApplication.java
@@ -0,0 +1,11 @@
+package com.example.battbatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattBattApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BattBattApplication.class, args);
+    }
+}
