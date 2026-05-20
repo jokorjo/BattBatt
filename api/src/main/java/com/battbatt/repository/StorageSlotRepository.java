8ca90423355d046e3ddb903049353ea4453466d4
jokorjo <joni.korkola@gmail.com>
2026-05-20 20:24:24 +0300

    Add StorageSlotRepository interface for JPA

---
+++ api/src/main/java/com/battbatt/repository/StorageSlotRepository.java
@@ -0,0 +1,7 @@
+package com.battbatt.repository;
+
+import com.battbatt.entity.StorageSlot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StorageSlotRepository extends JpaRepository<StorageSlot, Long> {
+}
