86f8fe3ed50e40c0d548f1a3b33e78fd7a9b15a6
jokorjo <joni.korkola@gmail.com>
2026-05-27 01:11:05 +0300

    Update StorageSlotRepository.java

--- api/src/main/java/com/battbatt/repository/StorageSlotRepository.java
+++ api/src/main/java/com/battbatt/repository/StorageSlotRepository.java
@@ -2,6 +2,12 @@ package com.battbatt.repository;
 
 import com.battbatt.entity.StorageSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StorageSlotRepository extends JpaRepository<StorageSlot, Long> {
+
+    // 🔥 HAE PROCESSING SLOT
+    @Query("SELECT s FROM StorageSlot s WHERE s.storage.name = :name")
+    StorageSlot findByStorageName(@Param("name") String name);
 }
