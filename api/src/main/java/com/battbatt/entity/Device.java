0f0a03b50409693859d040f8ae497b4f2457e5f8
jokorjo <joni.korkola@gmail.com>
2026-05-27 00:28:25 +0300

    Add Device entity with profiles relationship

---
+++ api/src/main/java/com/battbatt/entity/Device.java
@@ -0,0 +1,26 @@
+package com.battbatt.entity;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Device {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "device", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<DeviceProfile> profiles = new ArrayList<>();
+
+    public Long getId() { return id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public List<DeviceProfile> getProfiles() { return profiles; }
+    public void setProfiles(List<DeviceProfile> profiles) { this.profiles = profiles; }
+}
