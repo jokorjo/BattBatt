cff4ae8dbe22c5e0a043250510e5a4a3a0997664
jokorjo <joni.korkola@gmail.com>
2026-05-27 00:29:02 +0300

    Add DeviceProfile entity with attributes and methods

---
+++ api/src/main/java/com/battbatt/entity/DeviceProfile.java
@@ -0,0 +1,33 @@
+package com.battbatt.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class DeviceProfile {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private Device device;
+
+    private int minVoltage;
+    private int maxVoltage;
+
+    private double maxAmps;
+
+    public Long getId() { return id; }
+
+    public Device getDevice() { return device; }
+    public void setDevice(Device device) { this.device = device; }
+
+    public int getMinVoltage() { return minVoltage; }
+    public void setMinVoltage(int minVoltage) { this.minVoltage = minVoltage; }
+
+    public int getMaxVoltage() { return maxVoltage; }
+    public void setMaxVoltage(int maxVoltage) { this.maxVoltage = maxVoltage; }
+
+    public double getMaxAmps() { return maxAmps; }
+    public void setMaxAmps(double maxAmps) { this.maxAmps = maxAmps; }
+}
