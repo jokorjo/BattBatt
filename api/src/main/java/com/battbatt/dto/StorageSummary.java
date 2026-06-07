f8b7a4e64d74d274383c75ac84d107b8dd1ca223
jokorjo <joni.korkola@gmail.com>
2026-06-07 21:57:54 +0300

    Add utilization field to StorageSummary class

--- api/src/main/java/com/battbatt/dto/StorageSummary.java
+++ api/src/main/java/com/battbatt/dto/StorageSummary.java
@@ -6,16 +6,19 @@ public class StorageSummary {
     private String chemistry;
     private double totalWeight;
     private int batteryCount;
+    private double utilization;
 
     public StorageSummary(String storageName, String chemistry, double totalWeight, int batteryCount) {
         this.storageName = storageName;
         this.chemistry = chemistry;
         this.totalWeight = totalWeight;
         this.batteryCount = batteryCount;
+        this.utilization = utilization;
     }
 
     public String getStorageName() { return storageName; }
     public String getChemistry() { return chemistry; }
     public double getTotalWeight() { return totalWeight; }
     public int getBatteryCount() { return batteryCount; }
+    public double getUtilization() {return utilization; }
 }
