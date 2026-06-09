efeeec8e00297bbb55067ad3d78b7e26b2beeb00
jokorjo <joni.korkola@gmail.com>
2026-06-09 09:23:00 +0300

    Update BatteryController.java

--- api/src/main/java/com/battbatt/controller/BatteryController.java
+++ api/src/main/java/com/battbatt/controller/BatteryController.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 import com.battbatt.entity.StorageSlot;
 
+@CrossOrigin(origins = "https://batt-batt-pink.vercel.app")
 @RestController
 @RequestMapping("/api/batteries")
 @CrossOrigin
